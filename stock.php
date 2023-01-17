4535adf78386bab3ce49e3d8b166d98cf5d834f1
Aurelien-prog <aurelien.kostpro@gmail.com>
2023-01-17 08:29:14 +0100

    Finish

--- stock.php
+++ stock.php
@@ -99,7 +99,12 @@ if (isset($_POST['btn-search'])) {
                         <tr class="tr-table-stock">
                             <form action="traitement/edit.php" method="post" enctype="multipart/form-data">
                                 <input name="id" type='hidden' value="<?php echo $rec['Id'];?>"/>
-                                <td class="td-search-ref"><input name="newreference" class="input-edit" type="text" value="<?php echo $rec['reference'];?>"></input></td>
+                                <td class="td-search-ref">
+                                    <form action="./traitement/delete.php" method='post'>
+                                        <button class="btn-delete zoom" type="submit" name="btn-delete"><svg class="img-cross" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg></button>
+                                    
+                                    <input name="newreference" class="input-edit" type="text" value="<?php echo $rec['reference'];?>"></input>
+                                </td>
                                 <td class="td-search"><input name="newcouleur" class="input-edit" type="text" value="<?php echo $rec['couleur'];?>"></input></td>
                                 <td class="td-little"><input name="newquantite" class="input-edit" type="text" value="<?php echo $rec['quantite'];?>"></input></td>
                                 <td class="td-little"><input name="newcolisage" class="input-edit" type="text" value="<?php echo $rec['colisage'];?>"></input></td>
